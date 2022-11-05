60e47b86e2e6ba9a30e6abf15d68a2f07730adbd
EnyelSequeira <enyelsequeira@hotmail.com>
2022-11-06 00:16:20 +0800

    mantine not working as expected

--- app/root.tsx
+++ app/root.tsx
@@ -7,10 +7,21 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import { createEmotionCache, MantineProvider } from "@mantine/core";
 import { StylesPlaceholder } from "@mantine/remix";
 import { theme } from "./theme";
-
+import {
+  AppShell,
+  Burger,
+  Button,
+  Group,
+  MediaQuery,
+  Navbar,
+  Stack,
+  Text,
+  createEmotionCache,
+  MantineProvider,
+} from "@mantine/core";
+import { useState } from "react";
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
@@ -19,7 +30,51 @@ export const meta: MetaFunction = () => ({
 
 createEmotionCache({ key: "mantine" });
 
+function AppShellHeader({ opened, setOpened }: any) {
+  return (
+    <Group
+      p="sm"
+      sx={{ backgroundColor: "#0C0F40", width: "100%" }}
+      position="center"
+    >
+      <Button variant="subtle">Promoti</Button>
+      <Button>Sign Up</Button>
+      <Button variant="light">Log In</Button>
+      <Button variant="subtle">Game</Button>
+      <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+        <Burger
+          opened={opened}
+          onClick={() => setOpened((o: any) => !o)}
+          size="sm"
+          mr="xl"
+        />
+      </MediaQuery>
+    </Group>
+  );
+}
+
+function AppShellNavbar({ opened }: any) {
+  return (
+    <Navbar
+      p="md"
+      hiddenBreakpoint="sm"
+      hidden={!opened}
+      width={{ sm: 200, lg: 300 }}
+      sx={{ backgroundColor: "#01001A", borderRight: "none" }}
+    >
+      <Stack mt="xl" align="center">
+        <Button>Sign Usp</Button>
+        <Button variant="light">Log In</Button>
+        <Text>Promotions</Text>
+        <Text>Tournaments</Text>
+        <Text>Responsive Gaming</Text>
+      </Stack>
+    </Navbar>
+  );
+}
 export default function App() {
+  const [opened, setOpened] = useState(false);
+
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
       <html lang="en">
@@ -29,7 +84,20 @@ export default function App() {
           <Links />
         </head>
         <body>
-          <Outlet />
+          <AppShell
+            sx={{ backgroundColor: "#01001A" }}
+            padding={0}
+            navbar={<AppShellNavbar opened={opened} />}
+          >
+            <AppShellHeader opened={opened} setOpened={setOpened} />
+            <Stack style={{ border: "solid #4EACE8", borderRadius: 10 }} p="sm">
+              {Array(40)
+                .fill("Page content goes here...")
+                .map((str, i) => (
+                  <div key={i}>{str}</div>
+                ))}
+            </Stack>
+          </AppShell>
           <ScrollRestoration />
           <Scripts />
           <LiveReload />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,76 @@
-import { Text, Button, Stack } from "@mantine/core";
+import {
+  AppShell,
+  Burger,
+  Button,
+  Group,
+  MediaQuery,
+  Navbar,
+  Stack,
+  Text,
+} from "@mantine/core";
+
+import { useState } from "react";
+
+function AppShellHeader({ opened, setOpened }: any) {
+  return (
+    <Group
+      p="sm"
+      sx={{ backgroundColor: "#0C0F40", width: "100%" }}
+      position="center"
+    >
+      <Button variant="subtle">Promoti</Button>
+      <Button>Sign Up</Button>
+      <Button variant="light">Log In</Button>
+      <Button variant="subtle">Game</Button>
+      <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+        <Burger
+          opened={opened}
+          onClick={() => setOpened((o: any) => !o)}
+          size="sm"
+          mr="xl"
+        />
+      </MediaQuery>
+    </Group>
+  );
+}
+
+function AppShellNavbar({ opened }: any) {
+  return (
+    <Navbar
+      p="md"
+      hiddenBreakpoint="sm"
+      hidden={!opened}
+      width={{ sm: 200, lg: 300 }}
+      sx={{ backgroundColor: "#01001A", borderRight: "none" }}
+    >
+      <Stack mt="xl" align="center">
+        <Button>Sign Usp</Button>
+        <Button variant="light">Log In</Button>
+        <Text>Promotions</Text>
+        <Text>Tournaments</Text>
+        <Text>Responsive Gaming</Text>
+      </Stack>
+    </Navbar>
+  );
+}
 
 export default function Index() {
+  const [opened, setOpened] = useState(false);
+
   return (
-    <Stack align="center" mt={50}>
-      <Text size="xl" weight={500}>
-        Welcome to Mantine!
-      </Text>
-      <Button>Click the button</Button>
-    </Stack>
+    <AppShell
+      sx={{ backgroundColor: "#01001A" }}
+      padding={0}
+      navbar={<AppShellNavbar opened={opened} />}
+    >
+      <AppShellHeader opened={opened} setOpened={setOpened} />
+      <Stack style={{ border: "solid #4EACE8", borderRadius: 10 }} p="sm">
+        {Array(40)
+          .fill("Page content goes here...")
+          .map((str, i) => (
+            <div key={i}>{str}</div>
+          ))}
+      </Stack>
+    </AppShell>
   );
 }
